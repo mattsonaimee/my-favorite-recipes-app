44e47c329e3986400bb6aaed250f5c7deec77c37
amatt <amattson84@gmail.com>
2021-02-21 14:07:07 -0800

    created file and added dependency

---
+++ routes/author-api-routes.js
@@ -0,0 +1,16 @@
+            // Dependencies
+            var db = require("../models");
+
+
+
+
+
+
+            // ---------Below are templates for the api routes and functions. We can update to our project models when created. 
+            // aimee 2/21/21------------------------------------
+            
+            
+            
+            
+            
+          
