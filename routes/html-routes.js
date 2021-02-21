304cac474cddedba3fbc2dedc50083a3543aad25
amatt <amattson84@gmail.com>
2021-02-21 14:17:33 -0800

    .get route and function template for models data

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,5 +11,33 @@ var db = require("../models");
 // aimee 2/21/21------------------------------------
 
 
+module.exports = function(app) {
+
+    // GET route for getting all of the recipes
+    app.get("/api/posts", function(req, res) {
+      var query = {};
+      if (req.query.author_id) {
+        query.AuthorId = req.query.author_id;
+      }
+      // 1. Add a join here to include all of the Authors to these posts
+      db.Post.findAll({
+        where: query
+      }).then(function(dbPost) {
+        res.json(dbPost);
+      });
+    });
+  
+    // Get route for retrieving a single post
+    app.get("/api/posts/:id", function(req, res) {
+      // 2. Add a join here to include the Author who wrote the Post
+      db.Post.findOne({
+        where: {
+          id: req.params.id
+        }
+      }).then(function(dbPost) {
+        console.log(dbPost);
+        res.json(dbPost);
+      });
+    });
 
 
