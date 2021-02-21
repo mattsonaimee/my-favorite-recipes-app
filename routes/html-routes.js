62dc634c517a378ab2f7643947cf5e1308329741
amatt <amattson84@gmail.com>
2021-02-21 14:16:56 -0800

    Created file and required dependency.

---
+++ routes/html-routes.js
@@ -0,0 +1,15 @@
+// Dependencies
+var db = require("../models");
+
+// Routes
+
+
+
+
+
+// ---------Below are templates for the api routes and functions. We can update to our project models when created. 
+// aimee 2/21/21------------------------------------
+
+
+
+
