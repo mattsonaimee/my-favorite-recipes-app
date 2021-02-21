eea7a729edc446bc63e25ee113ee75be7f3917ba
amatt <amattson84@gmail.com>
2021-02-21 14:09:13 -0800

    Wrote .delete route and function

--- routes/author-api-routes.js
+++ routes/author-api-routes.js
@@ -36,6 +36,17 @@
                       res.json(dbAuthor);
                     });
                   });
+                  app.delete("/api/authors/:id", function(req, res) {
+                    db.Author.destroy({
+                      where: {
+                        id: req.params.id
+                      }
+                    }).then(function(dbAuthor) {
+                      res.json(dbAuthor);
+                    });
+                  });
+                
+                };
                 
             
                
