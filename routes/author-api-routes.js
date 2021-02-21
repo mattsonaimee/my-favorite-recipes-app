115fbc31e934d1df31e923280adf7c2d87ba42a6
amatt <amattson84@gmail.com>
2021-02-21 14:08:35 -0800

    Wrote template for .get routes and function

--- routes/author-api-routes.js
+++ routes/author-api-routes.js
@@ -13,4 +13,23 @@
             
             
             
-          
+            module.exports = function(app) {
+                app.get("/api/authors", function(req, res) {
+                  // 1. Add a join to include all of each Author's Posts
+                  db.Author.findAll({}).then(function(dbAuthor) {
+                    res.json(dbAuthor);
+                  });
+                });
+              
+                app.get("/api/authors/:id", function(req, res) {
+                  // 2; Add a join to include all of the Author's Posts here
+                  db.Author.findOne({
+                    where: {
+                      id: req.params.id
+                    }
+                  }).then(function(dbAuthor) {
+                    res.json(dbAuthor);
+                  });
+                });
+            
+               
