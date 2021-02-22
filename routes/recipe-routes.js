0cc629ebc7518807b5f18454590e01a737a3e5d3
Sam Rogers <samrogers15@gmail.com>
2021-02-22 15:31:45 -0800

    commiting updates to routes

---
+++ routes/recipe-routes.js
@@ -0,0 +1,30 @@
+const db = require('../models');
+
+module.exports = (app) => {
+    app.get('/api/recipes', (req, res) => {
+        db.Recipe.findAll({
+            include: [db.Image],
+        }).then((dbRecipe) => res.json(dbRecipe));
+    });
+
+    app.get('/api/recipes/:id', (req, res) => {
+        db.Recipe.findOne({
+            where: {
+                id: req.params.id,
+            },
+            include: [db.Image],
+        }).then((dbRecipe) => res.json(dbRecipe));
+    });
+
+    app.post('/api/recipes', (req, res) => {
+        db.Recipe.create(req.body).then((dbRecipe) => res.json(dbRecipe));
+    });
+
+    app.delete('/api/recipes/:id', (req, res) => {
+        db.Recipe.destroy({
+            where: {
+                id: req.params.id,
+            },
+        }).then((dbRecipe) => res.json(dbRecipe))
+    });
+};
