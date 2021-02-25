122dc2b5fcfc75244ce69a9ab01aa4b060a620d7
Josh Rehanek <joshrehanek@gmail.com>
2021-02-24 18:02:20 -0800

    eslint

--- models/recipe.js
+++ models/recipe.js
@@ -1,62 +1,61 @@
-const { Sequelize, Model, DataTypes } = require("sequelize");
-const sequelize = new Sequelize("sqlite::memory:");
+// const { Sequelize, Model, DataTypes } = require('sequelize');
+// const sequelize = new Sequelize('sqlite::memory:');
 
 module.exports = (sequelize, DataTypes) => {
-    const Recipe = sequelize.define('Recipe', {
-        name: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-        },
-        ingredients: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-        },
-        directions: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-        },
-        URL: {
-            type: DataTypes.TEXT,
-            allowNull: true,
-        },
-        vegetarian: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-        },
-        vegan: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-        },
-        gluten_free: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-        },
-        favorite_recipe: {
-            type: DataTypes.BOOLEAN,
-            allowNull: true,
-        },
-        add_to_shopping_list: {
-            type: DataTypes.BOOLEAN,
-            allowNull: true,
-        }
-    });
-    return Recipe;
+  const Recipe = sequelize.define('Recipe', {
+    name: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    ingredients: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    directions: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    URL: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    vegetarian: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    vegan: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    gluten_free: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    favorite_recipe: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true
+    },
+    add_to_shopping_list: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true
+    }
+  });
+  return Recipe;
 };
 
-(async () => {
-    await sequelize.sync({ force: true });
-    // Code here
-  })();
+// (async () => {
+//     await sequelize.sync({ force: true });
+//     // Code here
+//   })();
 
-const pizza = await Recipe.create({
-    name: "mushroom pizza",
-    ingredients: "flour, water, salt, oil, cheese, sauce, mushrooms",
-    directions: "make the dough, put the sauce, top it off, then bake that sucker!",
-    URL: "www.pizza.com",
-    vegetarian: true,
-    vegan: false,
-    gluten_free: false
-})
-console.log(pizza instanceof Recipe);
-console.log(pizza.name);
-
+// const pizza = await Recipe.create({
+//     name: "mushroom pizza",
+//     ingredients: "flour, water, salt, oil, cheese, sauce, mushrooms",
+//     directions: "make the dough, put the sauce, top it off, then bake that sucker!",
+//     URL: "www.pizza.com",
+//     vegetarian: true,
+//     vegan: false,
+//     gluten_free: false
+// })
+// console.log(pizza instanceof Recipe);
+// console.log(pizza.name);
