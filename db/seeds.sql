0a2b2c4919e26c0bdb683dffb8d6bd7c693826e2
amatt <amattson84@gmail.com>
2021-03-03 18:59:26 -0800

    Deleted comments

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,18 +11,7 @@ CREATE TABLE users(
 
 
 
--- CREATE TABLE authors(
---   id INTEGER(11) AUTO_INCREMENT NOT NULL,
---   firstName VARCHAR(100),
---   lastName VARCHAR(100),
---   PRIMARY KEY (id)
--- );
-
     INSERT INTO users
         (email, user_password)
     VALUES
         ('samrogers15@gmail.com', 'password2');
-
--- INSERT INTO recipes
---     (name, ingredients, directions, URL, vegetarian, vegan, gluten_free, favorite_recipe, add_to_shopping_list)
--- VALUES('lasagna', 'dough, sauce, cheese, pepperoni', 'Throw the stuff together and put it in the oven', 'our website', 0, 0, 0, 1, 1);
